c94d7daa128e8b48cc648787543d07abddc9e185
Krrish0221 <Krrish0221@users.noreply.github.com>
2026-01-31 11:15:44 +0530

    Fix next.config.ts: move outputFileTracingExcludes to top level

--- next.config.ts
+++ next.config.ts
@@ -5,19 +5,19 @@ const nextConfig: any = {
   productionBrowserSourceMaps: false,
   experimental: {
     optimizePackageImports: ['iconoir-react'],
-    outputFileTracingExcludes: {
-      '*': [
-        'node_modules/@swc/**/*',
-        'node_modules/@esbuild/**/*',
-        'node_modules/terser/**/*',
-        'node_modules/webpack/**/*',
-        'node_modules/eslint/**/*',
-        'node_modules/typescript/**/*',
-        'node_modules/iconoir-react/**/*',
-        'node_modules/@types/**/*',
-        'node_modules/sharp/**/*',
-      ],
-    },
+  },
+  outputFileTracingExcludes: {
+    '*': [
+      'node_modules/@swc/**/*',
+      'node_modules/@esbuild/**/*',
+      'node_modules/terser/**/*',
+      'node_modules/webpack/**/*',
+      'node_modules/eslint/**/*',
+      'node_modules/typescript/**/*',
+      'node_modules/iconoir-react/**/*',
+      'node_modules/@types/**/*',
+      'node_modules/sharp/**/*',
+    ],
   },
   async rewrites() {
     return [
